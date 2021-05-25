718dec627cb303801d6cc122267fb836fd1aa571
Yuhong <yuhongz9309@hstat.org>
2021-05-24 20:53:22 -0400

    create register procress using firebase

---
+++ app/src/main/java/com/example/jobfinder/User.java
@@ -0,0 +1,16 @@
+package com.example.jobfinder;
+
+public class User {
+
+    public String fullName, email;
+
+    public User() {
+
+    }
+
+    public User(String fn, String e) {
+        fullName = fn;
+        email = e;
+    }
+
+}
